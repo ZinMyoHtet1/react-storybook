a9b4981722d3caba39032f74e1b3af580969e065
ZinMyoHtet1 <zmhtetcompany@gmail.com>
2024-04-10 20:32:37 +0630

    add Chakra Button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,21 +1,25 @@
 import React from "react";
-// import { addDecorator } from "@storybook/react";
+import { makeDecorator } from "@storybook/addons";
 import Center from "../src/decorators/Center/Center";
+import { ChakraProvider, theme, CSSReset } from "@chakra-ui/react";
 export const parameters = {
   backgrounds: {
     default: "white",
   },
 };
 
-export default {
-  parameters: {
-    options: {
-      storySort: (a, b) =>
-        a.id === b.id
-          ? 0
-          : a.id.localeCompare(b.id, undefined, { numeric: true }),
-    },
-  },
-};
+export const decorators = [
+  (story) => (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      {story()}
+    </ChakraProvider>
+  ),
+];
 
-export const decorators = [(story) => <Center>{story()}</Center>];
+// makeDecorator();
+
+export const options = {
+  storySort: (a, b) =>
+    a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
+};
---
+++ src/components/Chakra/Button/Button.stories.js
@@ -0,0 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
+export default {
+  title: "Chakra/Button",
+  component: Button,
+};
+
+export const Success = () => <Button colorScheme="green">Success</Button>;
+
+export const Warning = () => <Button colorScheme="yellow">Warning</Button>;
